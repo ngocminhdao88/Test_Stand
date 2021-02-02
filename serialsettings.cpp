703e740952334808b3b47a498f4cde95b619bd86
Minh Dao <ngocminhdao88@gmail.com>
2021-02-02 14:17:34 +0100

    Finished implementing SerialSettings class

--- serialsettings.cpp
+++ serialsettings.cpp
@@ -1,6 +1,52 @@
+#include "ui_serialsettings.h"
 #include "serialsettings.h"
+#include <QSerialPortInfo>
 
-SerialSettings::SerialSettings()
+SerialSettings::SerialSettings(QWidget *parent)
+    : QDialog(parent),
+      ui(new Ui::SerialSettings)
 {
+    ui->setupUi(this);
 
+    //setup UI with default values
+    ui->cbxParity->setCurrentIndex(0);
+    ui->cbxBaudrate->setCurrentText(QString::number(parameters().baud));
+    ui->cbxDatabits->setCurrentText(QString::number(parameters().databits));
+    ui->cbxStopbits->setCurrentText(QString::number(parameters().stopbits));
+    ui->spxTimeout->setValue(parameters().timeout);
+    ui->spxRetry->setValue(parameters().retry);
+
+
+    //refresh serial port
+    connect(ui->btnPort, &QPushButton::clicked, [this] {
+        ui->cbxPort->clear();
+        QList<QSerialPortInfo> ports = QSerialPortInfo::availablePorts();
+        for(auto port: ports) {
+            ui->cbxPort->addItem(port.portName());
+        }
+    });
+
+    //save the settings
+    connect(ui->btnApply, &QPushButton::clicked, [this] {
+        m_parameters.portName = ui->cbxPort->currentText();
+        m_parameters.parity = ui->cbxParity->currentIndex();
+        if(m_parameters.parity > 0) m_parameters.parity++;
+        m_parameters.baud = ui->cbxBaudrate->currentText().toInt();
+        m_parameters.databits = ui->cbxDatabits->currentText().toInt();
+        m_parameters.stopbits = ui->cbxStopbits->currentText().toInt();
+        m_parameters.timeout = ui->spxTimeout->value();
+        m_parameters.retry = ui->spxRetry->value();
+
+        hide();
+    });
+}
+
+SerialSettings::~SerialSettings()
+{
+    delete ui;
+}
+
+SerialSettings::Parameters SerialSettings::parameters() const
+{
+    return m_parameters;
 }
--- serialsettings.h
+++ serialsettings.h
@@ -2,12 +2,37 @@
 #define SERIALSETTINGS_H
 
 #include <QObject>
+#include <QDialog>
+#include <QSerialPort>
 
-class SerialSettings
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class SerialSettings;
+}
+QT_END_NAMESPACE
+
+class SerialSettings : public QDialog
 {
     Q_OBJECT
 public:
-    SerialSettings();
+    SerialSettings(QWidget *parent = 0);
+    ~SerialSettings();
+
+    struct Parameters {
+        QString portName = "";
+        int parity = QSerialPort::NoParity;
+        int baud = QSerialPort::Baud9600;
+        int databits = QSerialPort::Data8;
+        int stopbits = QSerialPort::OneStop;
+        int timeout = 100;
+        int retry = 3;
+    };
+
+    Parameters parameters() const;
+
+private:
+    Parameters m_parameters;
+    Ui::SerialSettings *ui;
 };
 
 #endif // SERIALSETTINGS_H
